5b627c43366dd893bd0beb8130ebdc6711c72257
Aditya Kumar <adityakr365@gmail.com>
2022-03-11 09:51:51 +0530

    aading

---
+++ check.php
@@ -0,0 +1,60 @@
+
+<!DOCTYPE html>
+<html lang="en">
+<head>
+    <meta charset="UTF-8">
+    <meta http-equiv="X-UA-Compatible" content="IE=edge">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <title>Login</title>
+    <link rel="stylesheet" href="https://cdnjs.cloudflare.com/ajax/libs/font-awesome/5.15.3/css/all.min.css"/>
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <link rel="stylesheet" href="CSS/navbar.css">
+    <link rel="stylesheet" href="CSS/login.css">
+</head>
+<body>
+<nav>
+        <div class="logo">
+           MODULE
+        </div>
+        <input type="checkbox" id="click">
+        <label for="click" class="menu-btn">
+        <i class="fas fa-bars"></i>
+        </label>
+        <ul>
+           
+           <!-- <li><a href="#">ABOUT US</a></li> -->
+           <li><a href="register.php">Register</a></li>
+           <li><a class="active" href="login.php">Login</a></li>
+           <!-- <li><a href="#">Gallery</a></li>
+           <li><a href="#">Feedback</a></li> -->
+        </ul>
+     </nav>
+      <div class="content">
+        <h1 class="topheading"></h1>
+   
+        <div class="box" >
+            <h2 class="secondheading">UPLOAD DOCUMENT</h2>
+                    
+            <form action="upload.php" method="post" class="input1">
+                <div style="align-items: center;">
+                    <input type="file" name="file"  class="input topinput">
+                    <input type="text" name="description_entered" placeholder="discription" class="input">
+                    <input type="submit" class="submit" name="submit" value="Upload"/>
+    
+                </div>
+    
+            </form>
+        </div>
+        
+        <br>
+        <br>
+        <!-- <footer style="background-color: black; color:white;text-align:center;height:80px;">
+            <span ><br><div class="logo"><h4>
+               COPYRIGHT @ADITYA KUMAR</h4>
+             </div>
+            </span>
+            </footer> -->
+    </div>
+    
+</body>
+</html>
--- upload.php
+++ upload.php
@@ -1,61 +1,63 @@
-
-
 <!DOCTYPE html>
 <html lang="en">
 <head>
     <meta charset="UTF-8">
     <meta http-equiv="X-UA-Compatible" content="IE=edge">
     <meta name="viewport" content="width=device-width, initial-scale=1.0">
-    <title>Login</title>
-    <link rel="stylesheet" href="https://cdnjs.cloudflare.com/ajax/libs/font-awesome/5.15.3/css/all.min.css"/>
-    <meta name="viewport" content="width=device-width, initial-scale=1.0">
-    <link rel="stylesheet" href="CSS/navbar.css">
-    <link rel="stylesheet" href="CSS/login.css">
+    <title>Document</title>
 </head>
+
 <body>
-<nav>
-        <div class="logo">
-           MODULE
-        </div>
-        <input type="checkbox" id="click">
-        <label for="click" class="menu-btn">
-        <i class="fas fa-bars"></i>
-        </label>
-        <ul>
-           
-           <!-- <li><a href="#">ABOUT US</a></li> -->
-           <li><a href="register.php">Register</a></li>
-           <li><a class="active" href="login.php">Login</a></li>
-           <!-- <li><a href="#">Gallery</a></li>
-           <li><a href="#">Feedback</a></li> -->
-        </ul>
-     </nav>
-      <div class="content">
-        <h1 class="topheading"></h1>
-   
-        <div class="box" >
-            <h2 class="secondheading">UPLOAD DOCUMENT</h2>
-                    
-            <form action="upload.php" method="post" class="input1">
-                <div style="align-items: center;">
-                    <input type="file" name="file"  class="input topinput">
-                    <input type="text" name="description_entered" placeholder="discription" class="input">
-                    <input type="submit" class="submit" name="submit" value="Upload"/>
-    
-                </div>
-    
-            </form>
-        </div>
+
+<?php
+
+// $dbh= new PDO("mysql: host=localhost; dbname=module", "root","");
+if(isset($_POST['btn'])){
+    $servername = "localhost";
+    $username = "root";
+    $password = "";
+    $dbname = "module";
+    // echo"Username";
+// Create connection
+    $conn = new mysqli($servername, $username, $password, $dbname);
+// Check connection
+    if ($conn->connect_error) {
+        die("Connection failed: " . $conn->connect_error);
         
-        <br>
-        <br>
-        <!-- <footer style="background-color: black; color:white;text-align:center;height:80px;">
-            <span ><br><div class="logo"><h4>
-               COPYRIGHT @ADITYA KUMAR</h4>
-             </div>
-            </span>
-            </footer> -->
-    </div>
-    
+    }
+$name= $_FILES['myfile']['name'];
+$type = $_FILES['myfile']['type'];
+
+$data= file_get_contents($_FILES['myfile']['tmp_name']);
+
+$conn->query("INSERT INTO  myblob(name,mime)values('$name','$type')");
+// $stmt->bindParam(1,$name);
+// $stmt->bindParam(2,$type);
+// $stmt->bindParam(3,$data);
+// $stmt->execute();
+}
+
+?>
+<form method="post" enctype="multipart/form-data"> 
+    <input type="file" name="myfile"/>
+
+<button name="btn">Upload</button>
+
+</form>
+
+<p></p>
+
+<ol>
+
+<?php
+// $stat = $dbh->prepare ("select * from myblob");
+// $stat->execute();
+// while ($row = $stat->fetch()){
+//     echo "<li><a target='_blank' href='view.php?id=".$row['id']."'>".$row['name']."</a></li>";
+
+// }
+
+?>
+</ol>
 </body>
 </html>
