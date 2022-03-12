9f7195bf682006696cb3679817e5674404e174ad
Aditya Kumar <adityakr365@gmail.com>
2022-03-12 13:45:05 +0530

    adding

--- upload.php
+++ upload.php
@@ -9,37 +9,33 @@
 
 <body>
 
+
 <?php
 
-// $dbh= new PDO("mysql: host=localhost; dbname=module", "root","");
+$dbh =new PDO("mysql: host=localhost; dbname=module", "root", "");
+
 if(isset($_POST['btn'])){
-    $servername = "localhost";
-    $username = "root";
-    $password = "";
-    $dbname = "module";
-    // echo"Username";
-// Create connection
-    $conn = new mysqli($servername, $username, $password, $dbname);
-// Check connection
-    if ($conn->connect_error) {
-        die("Connection failed: " . $conn->connect_error);
-        
-    }
+
 $name= $_FILES['myfile']['name'];
+
 $type = $_FILES['myfile']['type'];
 
-$data= file_get_contents($_FILES['myfile']['tmp_name']);
+$data = file_get_contents($_FILES['myfile']['tmp_name']);
+$stmt = $dbh->prepare ("insert into myblob values('',?,?,?)");
 
-$conn->query("INSERT INTO  myblob(name,mime)values('$name','$type')");
-// $stmt->bindParam(1,$name);
-// $stmt->bindParam(2,$type);
-// $stmt->bindParam(3,$data);
-// $stmt->execute();
+$stmt->bindParam (1, $name);
+
+$stmt->bindParam (2,$type);
+
+$stmt->bindParam (3, $data);
+
+$stmt->execute();
+
 }
 
 ?>
-<form method="post" enctype="multipart/form-data"> 
-    <input type="file" name="myfile"/>
+
+<form method="post" enctype="multipart/form-data"> <input type="file" name="myfile"/>
 
 <button name="btn">Upload</button>
 
@@ -50,14 +46,23 @@ $conn->query("INSERT INTO  myblob(name,mime)values('$name','$type')");
 <ol>
 
 <?php
-// $stat = $dbh->prepare ("select * from myblob");
-// $stat->execute();
-// while ($row = $stat->fetch()){
-//     echo "<li><a target='_blank' href='view.php?id=".$row['id']."'>".$row['name']."</a></li>";
 
-// }
+$stat = $dbh->prepare ("select * from myblob");
+
+$stat->execute();
+
+while ($row = $stat->fetch()){
+
+echo "<li><a target='_blank '[href='view.php?id=".$row['id'].">".$row['name']."</a></li>";
+
+}
 
 ?>
+
 </ol>
+
+</body>
+
+</html>
 </body>
 </html>
